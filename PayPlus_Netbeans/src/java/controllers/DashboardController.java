bad2c9f959942bcfbd41f61ba413686e16f00b6f
Bryant Jonathan Gietosusilo <ianianrendy@gmail.com>
2025-01-05 01:02:32 +0700

    fix conflict

--- PayPlus_Netbeans/src/java/controllers/DashboardController.java
+++ PayPlus_Netbeans/src/java/controllers/DashboardController.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controllers;
 
 import models.ExpenseRecord;
 import models.IncomeRecord;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,44 +11,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.Savings;
 
-/**
- *
- * @author fauss
- */
 @WebServlet(name = "DashboardController", urlPatterns = {"/Dashboard"})
 public class DashboardController extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-<<<<<<< HEAD
-            throws ServletException, IOException {       
+            throws ServletException , IOException {
         if (request.getSession().getAttribute("currPhone") == null) {
             response.sendRedirect("User?menu=login");
         } else {
             Object phone = request.getSession().getAttribute("currPhone");
+
             ArrayList<ArrayList<Object>> dataExpense = new ExpenseRecord().query(
                     "SELECT SUM(amount) FROM expense WHERE phone = " + phone);
-            ArrayList<ArrayList<Object>> dataIncome = new ExpenseRecord().query(
+            ArrayList<ArrayList<Object>> dataIncome = new IncomeRecord().query(
                     "SELECT SUM(amount) FROM income WHERE phone = " + phone);
+            Savings where = new Savings();
+            where.where("phone = 0");
+            ArrayList<Savings> saves = where.get();
+            request.getSession().setAttribute("list", saves);
             request.setAttribute("dataIncome", dataIncome);
             request.setAttribute("dataExpense", dataExpense);
             request.getRequestDispatcher("Pages/DashboardPage.jsp").forward(request, response);
         }
-=======
-            throws ServletException, IOException {
-        String phone = "83824099809";
-
-        ArrayList<ArrayList<Object>> dataExpense = new ExpenseRecord().query(
-                "SELECT SUM(amount) FROM expense WHERE phone = " + phone);
-        ArrayList<ArrayList<Object>> dataIncome = new ExpenseRecord().query(
-                "SELECT SUM(amount) FROM income WHERE phone = " + phone);
-        Savings where = new Savings();
-        where.where("phone = 0");
-        ArrayList<Savings> saves = where.get();
-        request.getSession().setAttribute("list", saves);
-        request.setAttribute("dataIncome", dataIncome);
-        request.setAttribute("dataExpense", dataExpense);
-        request.getRequestDispatcher("Pages/DashboardPage.jsp").forward(request, response);
->>>>>>> 30ffcd8c137768ad110b0175573e64cbd18a4067
     }
 }
--- PayPlus_Netbeans/src/java/controllers/IncomeController.java
+++ PayPlus_Netbeans/src/java/controllers/IncomeController.java
@@ -12,20 +12,19 @@ import java.util.ArrayList;
 
 @WebServlet(name = "IncomeController", urlPatterns = {"/Income"})
 public class IncomeController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-<<<<<<< HEAD
         if (request.getSession().getAttribute("currPhone") == null) {
             response.sendRedirect("User?menu=login");
         } else {
             Object phone = request.getSession().getAttribute("currPhone");
             ArrayList<ArrayList<Object>> dataIncome = new IncomeRecord().query(
                     "SELECT SUM(amount), "
-                            + "COUNT(*), "
-                            + "SUM(CASE WHEN type = 'normal' THEN amount ELSE 0 END), "
-                            + "SUM(CASE WHEN type = 'gift' THEN amount ELSE 0 END) "
-                            + "FROM income WHERE phone ="+phone);
+                    + "COUNT(*), "
+                    + "SUM(CASE WHEN type = 'normal' THEN amount ELSE 0 END), "
+                    + "SUM(CASE WHEN type = 'gift' THEN amount ELSE 0 END) " + "FROM income WHERE phone =" + phone);
             request.setAttribute("dataIncome", dataIncome);
 
             String filter = request.getParameter("filter");
@@ -33,48 +32,21 @@ public class IncomeController extends HttpServlet {
             if (request.getParameterMap().isEmpty()) { // view all
                 conds.where("phone = " + phone);
                 ArrayList<IncomeRecord> incomeList = conds.get();
-                request.setAttribute("IncomeList", incomeList); 
+                request.setAttribute("IncomeList", incomeList);
             } else if ("normal".equals(filter)) { //view filter normal
                 conds.where("phone = " + phone + " AND type = 'normal'");
                 ArrayList<IncomeRecord> incomeList = conds.get();
-                request.setAttribute("IncomeList", incomeList); 
+                request.setAttribute("IncomeList", incomeList);
             } else if ("gift".equals(filter)) { // view filter gift
                 conds.where("phone = " + phone + " AND type = 'gift'");
                 ArrayList<IncomeRecord> incomeList = conds.get();
-                request.setAttribute("IncomeList", incomeList); 
+                request.setAttribute("IncomeList", incomeList);
+            } else if ("topup".equals(filter)) { // view filter gift
+                conds.where("phone = " + phone + " AND type = 'topup'");
+                ArrayList<IncomeRecord> incomeList = conds.get();
+                request.setAttribute("IncomeList", incomeList);
             }
             request.getRequestDispatcher("Pages/IncomePage.jsp").forward(request, response);
         }
-=======
-        String phone = "838240998098888";
-        
-        ArrayList<ArrayList<Object>> dataIncome = new IncomeRecord().query(
-                "SELECT SUM(amount), "
-                        + "COUNT(*), "
-                        + "SUM(CASE WHEN type = 'normal' THEN amount ELSE 0 END), "
-                        + "SUM(CASE WHEN type = 'gift' THEN amount ELSE 0 END) "+"FROM income WHERE phone ="+phone);
-        request.setAttribute("dataIncome", dataIncome);
-        
-        String filter = request.getParameter("filter");
-        IncomeRecord conds = new IncomeRecord();
-        if (request.getParameterMap().isEmpty()) { // view all
-            conds.where("phone = " + phone);
-            ArrayList<IncomeRecord> incomeList = conds.get();
-            request.setAttribute("IncomeList", incomeList); 
-        } else if ("normal".equals(filter)) { //view filter normal
-            conds.where("phone = " + phone + " AND type = 'normal'");
-            ArrayList<IncomeRecord> incomeList = conds.get();
-            request.setAttribute("IncomeList", incomeList); 
-        } else if ("gift".equals(filter)) { // view filter gift
-            conds.where("phone = " + phone + " AND type = 'gift'");
-            ArrayList<IncomeRecord> incomeList = conds.get();
-            request.setAttribute("IncomeList", incomeList); 
-        }else if ("topup".equals(filter)) { // view filter gift
-            conds.where("phone = " + phone + " AND type = 'topup'");
-            ArrayList<IncomeRecord> incomeList = conds.get();
-            request.setAttribute("IncomeList", incomeList); 
-        }
-        request.getRequestDispatcher("Pages/IncomePage.jsp").forward(request, response);
->>>>>>> 30ffcd8c137768ad110b0175573e64cbd18a4067
     }
 }
